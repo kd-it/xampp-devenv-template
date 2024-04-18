05d9930b9644682ffb5d489cd2638d27cdf8babc
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2024-04-19 04:16:28 +0900

    動く目処の立った版[WIP]

---
+++ tests/TestAccess.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
+import unittest
+import sys
+
+# リモートサーバーのアドレス
+REMOTE_URL = 'http://selenium:4444/wd/hub'
+
+class SampleTest(unittest.TestCase):
+    def setUp(self):
+            self.driver = webdriver.Remote(REMOTE_URL, options=webdriver.ChromeOptions())
+            assert self.driver is not None
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_access(self):
+        self.driver.get('http://web/')
+        time.sleep(2)
+        self.driver.save_screenshot('screenshot.png')
+
+    def test_title(self):
+        self.driver.get('http://web/')
+        time.sleep(2)
+        self.assertTrue('PHP' in self.driver.title)
+
+    def test_title_PHP(self):
+        self.driver.get('http://web/')
+        time.sleep(2)
+        self.assertTrue('PHP' in self.driver.title)
+
+if __name__ == '__main__':
+    unittest.main()
