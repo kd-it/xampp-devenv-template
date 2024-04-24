549c85a42f44cf1a31587363e07f7544c11898c7
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2024-04-24 16:13:41 +0900

    ヘルスチェックがまともに機能するようにした(DB)

--- db/build/healthcheck.sh
+++ db/build/healthcheck.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+
 # Wait for the database to be ready
-mysqladmin ping -h localhost --silent -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --wait=5
+mysqladmin ping -h localhost --silent -h db \
+    -u "${MYSQL_USER}" --password="${MYSQL_PASSWORD}" \
+    --wait=1
