89a899d2105855fc427a5168a120b5efb25c99e6
SATO Daisuke <densuke@fuga.jp>
2025-05-28 06:12:38 +0900

    日時確認(日本語表記)

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@
         このページは <?= $os ?> (<?= $arch ?>) で動作しています。
     </p>
     <ul>
+        <li>現在の日本時間は <?php echo date('Y-m-d H:i:s'); ?> です。</li>
         <li><a href="test.php">test.php(DB接続テスト)</a></li>
         <li><a href="info.php">info.php(phpinfo)</a></li>
     </ul>
